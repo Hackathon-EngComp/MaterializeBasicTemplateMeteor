4d52106de85dd09c7012914cf38f67c15ee4605b
eduardo-cess <eduardo_cess@yahoo.com.br>
2016-03-24 01:05:23 -0300

    inclusão do getJson na api do Siconv

--- controller/main.js
+++ controller/main.js
@@ -1,7 +1,16 @@
 
 if (Meteor.isClient) {
 
-
+Template.home.onRendered(function(){
+     $.getJSON("http://api.convenios.gov.br/siconv/v1/consulta/convenios.json?uf=pa",function (data) {
+        //dados = data.convenios;
+        console.log(data);
+        // for (var i = dados.length - 1; i >= 0; i--) {
+        //   $("ol").append('<li>'+dados[i].valor_global+'</li>');
+        // }
+      }              
+    );
+  })
 
 
 }
