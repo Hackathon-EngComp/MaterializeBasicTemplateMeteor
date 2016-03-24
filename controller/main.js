26c95c01e5347880a5972bce7f4a1ed540c425bd
eduardo-cess <eduardo_cess@yahoo.com.br>
2016-03-24 13:53:36 -0300

    tirei o rodapé por se tratar de um app mobile, adicionei o primeiro filtro (quantidade de convênios por estado)

--- controller/main.js
+++ controller/main.js
@@ -1,16 +1,24 @@
 
 if (Meteor.isClient) {
 
-Template.home.onRendered(function(){
-     $.getJSON("http://api.convenios.gov.br/siconv/v1/consulta/convenios.json?uf=pa",function (data) {
-        //dados = data.convenios;
-        console.log(data);
-        // for (var i = dados.length - 1; i >= 0; i--) {
-        //   $("ol").append('<li>'+dados[i].valor_global+'</li>');
-        // }
-      }              
-    );
-  })
+	var dados = new Array();
 
+	Template.home.onRendered(function(){
+	    $('select').material_select();
+	     	
+	  });
 
+	Template.home.events({
+		'change select': function () {
+			var uf = $('select').val();
+			
+			$(".progress").show();
+			$("#result").hide();
+			$.getJSON("http://api.convenios.gov.br/siconv/v1/consulta/convenios.json?uf="+uf, function(data){
+				$(".progress").hide();
+				$("#result").html("<p>O total de convênios: "+data.metadados.total_registros+"</p>");
+				$("#result").show();
+			});
+		}
+	});
 }
