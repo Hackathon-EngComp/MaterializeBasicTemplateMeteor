a3501b7cc6d071015b3c810cd9b019de65925744
eduardo-cess <eduardo_cess@yahoo.com.br>
2016-03-24 22:40:38 -0300

    Foi criada a consulta de valor total de convênios por estado

--- controller/main.js
+++ controller/main.js
@@ -1,8 +1,13 @@
 
 if (Meteor.isClient) {
 
-	var dados = new Array();
-
+	var convenios = new Array();
+	var allConvenios = new Array();
+	var offset = 0;
+	var valorTotalConvenios = .0;
+	var cont = 0;
+	var totalConvenios = 0;
+	var uf = '';
 	Template.home.onRendered(function(){
 	    $('select').material_select();
 	     	
@@ -10,15 +15,103 @@ if (Meteor.isClient) {
 
 	Template.home.events({
 		'change select': function () {
-			var uf = $('select').val();
-			
-			$(".progress").show();
-			$("#result").hide();
+			prepararTela();
+
 			$.getJSON("http://api.convenios.gov.br/siconv/v1/consulta/convenios.json?uf="+uf, function(data){
+				totalConvenios = data.metadados.total_registros;
+				$("#result").append("<p>Quantidade Total de convênios: "+totalConvenios+"</p>");
+				convenios.push(data.convenios);
+
+				if(totalConvenios > 500){
+					while (offset < totalConvenios) {
+						offset += 500;
+						
+						allConvenios = getRestoConvenios(uf, offset);
+						var data = $.parseJSON(allConvenios);
+						convenios.push(data.convenios);
+
+						for (var i = 0; i < convenios.length; i++) {
+							for (var j = 0; j < convenios[i].length; j++) {
+								valorTotalConvenios += parseFloat(convenios[i][j].valor_global);
+								cont++;
+							}
+						}
+					}
+				}
+				$("#result").append("<p>Valor total de convênios: "+converteFloatMoeda(valorTotalConvenios)+"</p>");
 				$(".progress").hide();
-				$("#result").html("<p>O total de convênios: "+data.metadados.total_registros+"</p>");
-				$("#result").show();
+				$('select').show();
 			});
 		}
 	});
+
+	function getRestoConvenios(uf, offset){
+		var value= $.ajax({ 
+		       url: "http://api.convenios.gov.br/siconv/v1/consulta/convenios.json?uf="+uf+"&offset="+offset, 
+		       async: false
+		    }).responseText;
+		    return value;
+	}
+
+	function returnConvenios(Convenios){
+		return Convenios;
+	}
+
+	function converteFloatMoeda(valor){
+      var inteiro = null, decimal = null, c = null, j = null;
+      var aux = new Array();
+      valor = ""+valor;
+      c = valor.indexOf(".",0);
+      //encontrou o ponto na string
+      if(c > 0){
+         //separa as partes em inteiro e decimal
+         inteiro = valor.substring(0,c);
+         decimal = valor.substring(c+1,valor.length);
+      }else{
+         inteiro = valor;
+      }
+      
+      //pega a parte inteiro de 3 em 3 partes
+      for (j = inteiro.length, c = 0; j > 0; j-=3, c++){
+         aux[c]=inteiro.substring(j-3,j);
+      }
+      
+      //percorre a string acrescentando os pontos
+      inteiro = "";
+      for(c = aux.length-1; c >= 0; c--){
+         inteiro += aux[c]+'.';
+      }
+      //retirando o ultimo ponto e finalizando a parte inteiro
+      
+      inteiro = inteiro.substring(0,inteiro.length-1);
+      
+      decimal = parseInt(decimal);
+      if(isNaN(decimal)){
+         decimal = "00";
+      }else{
+         decimal = ""+decimal;
+         if(decimal.length === 1){
+            decimal = decimal+"0";
+         }
+      }
+      
+      
+      valor = "R$ "+inteiro+","+decimal;
+      
+      
+      return valor;
+	}
+
+	function prepararTela(){
+		convenios.length = 0;
+		allConvenios.length = 0;
+		offset = 0;
+		valorTotalConvenios = .0;
+		cont = 0;
+		totalConvenios = 0;
+		$("#result").html("");
+		uf = $('select').val();
+		$('select').hide();			
+		$(".progress").show();
+	}
 }
